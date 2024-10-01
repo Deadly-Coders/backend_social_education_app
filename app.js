8b959513a8e8055e376ae9317d752c84bdb336ac
Shubham <shubhamkr1177@gmail.com>
2024-10-01 17:23:08 +0530

    Added security features

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const cors = require('cors');
 
 const userRouter = require('./routes/userRoutes');
 const marketRouter = require('./routes/marketRoutes');
@@ -11,8 +12,40 @@ const socketRouter = require('./routes/socketRouter');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController'); //ERROR HANDLERS
 
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const compression = require('compression');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean'); //security based middlewares
+
+app.use(cors());
+app.options('*', cors());
+
+//SECURITY MIDDLEWARES
+
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    crossOriginEmbedderPolicy: false,
+    crossOriginOpenerPolicy: false,
+  })
+);
+
 app.use(morgan('dev')); //morgan middleware
 app.use(express.json({ limit: '10kb' }));
+
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
+app.use(compression());
+const limiter = rateLimit({
+  //PREVENTS FROM DDOS
+  max: 100, // No. of requests
+  windowMs: 60 * 60 * 1000, // Time in Ms
+  message: 'Too many requests from this IP, Please try again in an hour', // Error message
+});
+app.use('/api', limiter);
 
 app.use(express.static(`${__dirname}/view`));
 
